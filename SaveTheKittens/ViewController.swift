6e74e3f541ab040d49b98ff642b95aef0e93b3c5
Zoya Slavina <inifnityformi@gmail.com>
2020-08-05 13:08:55 +0200

    Initial Commit

---
+++ SaveTheKittens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaveTheKittens
+//
+//  Created by Zoya Slavia on 05/08/2020.
+//  Copyright © 2020 Zoya Slavia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
